64fbed93e8a4ca75a9a1c8b868163014818edd67
Dias <wendson.bruno.dias@accenture.com>
2019-09-02 11:29:27 -0300

    alterações Scripts pdf Maker

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './jsPDF/app.component';
 import { PdfMakerComponent } from './pdf-maker/pdf-maker.component';
-
 import { PdfmakeService } from 'ng-pdf-make';
 @NgModule({
   declarations: [
